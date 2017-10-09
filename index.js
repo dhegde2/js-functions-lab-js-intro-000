dc0e4e16d007dcc167648e3b2ae6b028c1a2bd31
Dhruv Hegde <desidhruv@gmail.com>
2017-10-09 15:13:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ return "Happy holidays!";
 }
 
 function happyHolidaysTo(string) {
-return "Happy holidays, " + string;
+return "Happy holidays, " + string + "!";
 }
 
 function happyHolidayTo(string1, string2){
-return "Happy " + string1 + ", " + string2;
+return "Happy " + string1 + ", " + string2 + "!";
 }
 
 function HolidayCountdown(string, int) {
-return "It's " + int + "days until " + string;
+return "It's " + int + "days until " + string + "!";
 }
